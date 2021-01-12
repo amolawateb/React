eaae13df24f973dfcfdf5f70e1fb9efed0753e80
Amol Awate <amolawateb@gmail.com>
2021-01-12 20:51:30 +0000

    class, property and context component

--- demo-project/src/index.js
+++ demo-project/src/index.js
@@ -36,7 +36,7 @@ const element=(
 ReactDOM.render(element,document.getElementById('root'));*/
 
 //function compnenet------------------
-var DisplayEmployeeInfo = (employee) => {
+/*var DisplayEmployeeInfo = (employee) => {
   return <div>
     <h1>Employee Details</h1>
     <p>
@@ -70,15 +70,22 @@ const elementEmployee = <DisplayEmployeeInfo Id="100" Name="Amol" Location="Dubl
   Salary="45000" DeptId="1" DeptName="IT">
 </DisplayEmployeeInfo>
 
-ReactDOM.render(elementEmployee, document.getElementById("employee"));
+ReactDOM.render(elementEmployee, document.getElementById("employee"));*/
 //------------
 
 //class compnent--------
 class Employees extends React.Component {
   constructor(props) {
     super(props);
-    console.log(this.props);
+    this.state = {
+      updatedSalary: null
+    };
   }
+
+  getUpdatedSalary = (newSalary) => {
+    this.setState({ updatedSalary: newSalary });
+  }
+
   render() {
     return <div>
       <h1>Employee Class Details</h1>
@@ -94,7 +101,49 @@ class Employees extends React.Component {
       <p>
         <label>Employee Salary: <b>{this.props.Salary}</b></label>
       </p>
+      <p><label>Updated Sal:<b>{this.state.updatedSalary}</b></label></p>
+
       <Departments dId={this.props.DeptId} dName={this.props.DeptName}></Departments>
+      <Salary BasicSalary={this.props.BasicSalary}
+        HRA={this.props.HRA}
+        SpclAll={this.props.SpclAll}
+        onSalaryChanged={this.getUpdatedSalary}></Salary>
+    </div>
+  }
+}
+
+class Salary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.BasicRef = React.createRef();
+    this.HraRef = React.createRef();
+    this.SaRef = React.createRef();
+    this.state = {
+      basic: this.props.BasicSalary,
+      hrs: this.props.HRA,
+      sa: this.props.SpclAll
+    };
+  }
+  updateSalary = () => {
+    let newSalary = parseInt(this.BasicRef.current.value) +
+      parseInt(this.HraRef.current.value) +
+      parseInt(this.SaRef.current.value);
+
+    this.props.onSalaryChanged(newSalary);
+  }
+  render() {
+    return <div>
+      <h1>Salary Details</h1>
+      <p>
+        <label>Basic Salary: <input type="text" ref={this.BasicRef} defaultValue={this.state.basic}></input></label>
+      </p>
+      <p>
+        <label>HRA: <input type="text" ref={this.HraRef} defaultValue={this.state.hrs} ></input></label>
+      </p>
+      <p>
+        <label>Special Allowance: <input type="text" ref={this.SaRef} defaultValue={this.state.sa} ></input></label>
+      </p>
+      <button onClick={this.updateSalary}>Update</button>
     </div>
   }
 }
@@ -107,12 +156,15 @@ class Departments extends React.Component {
     </div>
   }
 }
-const classEmployee = <Employees Id="100" Name="Carol" Location="Cork" Salary="50000" DeptId="200" DeptName="BA"></Employees>
+
+const classEmployee = <Employees Id="100" Name="Carol" Location="Cork" Salary="50000"
+  DeptId="200" DeptName="BA"
+  BasicSalary="48000" HRA="1000" SpclAll="1000"></Employees>
 ReactDOM.render(classEmployee, document.getElementById("classEmployee"));
 //----------------
 
 //state management----------
-class State extends React.Component {
+/*class State extends React.Component {
   state={counter:0};
   addEmployee=()=>{
     this.setState({counter:this.state.counter+1});
@@ -145,5 +197,73 @@ class CountChar extends React.Component {
 }
 
 const countChar = <CountChar></CountChar>
-ReactDOM.render(countChar, document.getElementById("countChar"));
+ReactDOM.render(countChar, document.getElementById("countChar"));*/
+//--------------------
+
+//---context Compnenets
+
+const employeeContext = React.createContext();
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Id: 101,
+      Name: 'Amol',
+      Location: 'Dublin',
+      Salary: 45000
+    };
+  }
+  chnageEmployeeData=()=>
+  {
+    this.setState({Id:102, Name:'Carol',Location:'Cork', Salary:50000});
+  }
+  render() {
+    return <div>
+      <h1>Welcome to App Components</h1>
+      <p>
+      </p>
+      <employeeContext.Provider value={this.state}>
+        <Employees1></Employees1>
+      </employeeContext.Provider>
+      <p>
+        <button onClick={this.chnageEmployeeData}>Update Employee</button>
+      </p>
+    </div>
+  }
+}
+class Employees1 extends React.Component {
+  static contextType = employeeContext;
+  render() {
+    return <div>
+      <h1>Welcome to Employee Component</h1>
+      <p>
+        <label>Id: <b>{this.context.Id}</b></label>
+      </p>
+      <p>
+        <label>Name: <b>{this.context.Name}</b></label>
+      </p>
+      <p>
+        <label>Location: <b>{this.context.Location}</b></label>
+      </p>
+      <Salary1></Salary1>
+    </div>
+  }
+}
+
+class Salary1 extends React.Component {
+  static contextType = employeeContext;
+  render() {
+    return <div>
+      <h1>Welcome to Salary Component</h1>
+      <p>
+        <label>Salary: <b>{this.context.Salary}</b></label>
+      </p>
+    </div>
+  }
+}
+
+const app = <App></App>
+ReactDOM.render(app, document.getElementById("app"));
+
 //--------------------
